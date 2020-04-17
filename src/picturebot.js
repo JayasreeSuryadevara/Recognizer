c7585d4fcf1f77736111bd1b45034db30c3653bb
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-17 10:34:29 -0700

    CSS on main page

--- src/picturebot.js
+++ src/picturebot.js
@@ -1,10 +1,8 @@
 const SIZE = 300;
-const THRESHOLD = 60;
-const OBJECT_PROP = null;
-const OBSERVATIONS = [];
-const OBS_COUNT = 0;
-const DIMENSIONS = 2;
-
+const THRESHOLD = 150;
+const LEARNING_DATA = [];
+let OBJ_PROP = null;
+let OBJ_COUNT = 0;
 
 export default class PictureBot {
   constructor(canvas) {
@@ -38,11 +36,9 @@ export default class PictureBot {
   }
 
   drawImage(fileList) {
-    // const output = document.getElementById('output');
     let file = null;
     let imageURL = null;
     for (let i = 0; i < fileList.length; i++) {
-      console.log("type",fileList[i].type )
       if (fileList[i].type.match(/^image\//)) {
         file = fileList[i];
         break;
@@ -58,11 +54,18 @@ export default class PictureBot {
     let context = this.canvas.getContext('2d');
     let image = new Image();
     image.onload = function() {
-      context.drawImage(image, 0, 0, SIZE, SIZE);     
+      // To adjust image aspect ratio to browser
+      debugger;
+      let min = Math.min(image.width, image.height);
+
+      let startX = (image.width - min) / 2;
+      let startY = (image.height - min) / 2;
+
+      context.drawImage(image, startX, startY, min, min, 0, 0, SIZE, SIZE);     
     }
     image.src = imageURL;
-    let matrix = this.getPixelMatrix(image.data);
-    debugger;
+    let pixelArr = context.getImageData( 0, 0, SIZE, SIZE);
+    let matrix = this.getGreyScaleMatrix(pixelArr);
     this.processMatrix(matrix);
   }
 
@@ -72,11 +75,13 @@ export default class PictureBot {
       alert("Enter a name for this object.");
       return;
     }
-    OBS_COUNT++;
-    OBSERVATIONS[OBS_COUNT] = {
+    OBJ_COUNT++;
+    LEARNING_DATA[OBJ_COUNT] = {
       name: name,
-      prop: OBJECT_PROP
+      prop: OBJ_PROP
     }
+    console.log(LEARNING_DATA);
+    document.getElementById("output").value = name;
     document.getElementById("image-name").value = "";
   }
 
@@ -87,22 +92,23 @@ export default class PictureBot {
   }
 
   processMatrix(matrix) {
-    debugger;
     this.isolateObject(matrix);
     let box = this.getBoundingBox(matrix);
     let boxProp = this.getBoxProperties(box);
-
+debugger;
     let blackPixels = this.countBlackPixels(matrix);
+    console.log("black pixels ", blackPixels)
     let boxArea = boxProp.width * boxProp.length;
+    console.log("box area", boxArea);
     let fullness = blackPixels / boxArea;
+    console.log("aspect ratio", boxProp.aspectRatio);
+    // OBJ_PROP = boxProp.aspectRatio;
 
-    OBJECT_PROP = boxProp.aspectRatio;
+    OBJ_PROP = [];
+    OBJ_PROP[1] = boxProp.aspectRatio;
+    OBJ_PROP[2] = fullness;
 
-    OBJECT_PROP = [];
-    OBJECT_PROP[1] = boxProp.aspectRatio;
-    OBJECT_PROP[2] = fullness;
-
-    this.recognize(OBJECT_PROP);
+    this.recognize(OBJ_PROP);
 
     this.updateCanvas(matrix);
     this.drawBox(box);
@@ -122,8 +128,8 @@ export default class PictureBot {
 
   recognize(currentObject) {
     let name;
-    if (OBS_COUNT == 0) {
-      name = '?';
+    if (OBJ_COUNT == 0) {
+      name = "Not Sure";
     } else {
       let neighbor = this.getNearestNeighbor(currentObject);
       name = neighbor.name;
@@ -134,12 +140,12 @@ export default class PictureBot {
   getNearestNeighbor(currentObject) {
     let neighbor = null;
     let minDist = null;
-    for (let i = 1; i <= OBS_COUNT; i++) {
-      let dist = Math.abs(currentObject - OBSERVATIONS[i].prop);
-      dist = this.distance(currentObject, OBSERVATIONS[i].prop);
+    for (let i = 1; i <= OBJ_COUNT; i++) {
+      let dist = Math.abs(currentObject - LEARNING_DATA[i].prop);
+      dist = this.distance(currentObject, LEARNING_DATA[i].prop);
       if (minDist == null || minDist > dist) {
         minDist = dist;
-        neighbor = OBSERVATIONS[i];
+        neighbor = LEARNING_DATA[i];
       }
     }
     return neighbor;
@@ -147,7 +153,7 @@ export default class PictureBot {
 
   distance(p1, p2) {
     let dist = 0;
-    for (let i = 1; i <= DIMENSIONS; i++) {
+    for (let i = 1; i <= 2; i++) {
       dist += (p1[i] - p2[i]) * (p1[i] - p2[i]);
     }
     return Math.sqrt(dist);
@@ -159,7 +165,8 @@ export default class PictureBot {
       width: 0,
       aspectRatio: 0
     }
-
+    debugger;
+    //Calculate the actual lenth and width of the image
     let deltaX = box.xMax - box.xMin + 1;
     let deltaY = box.yMax - box.yMin + 1;
 
@@ -171,25 +178,25 @@ export default class PictureBot {
   }
 
   getBoundingBox(matrix) {
-    let bbox = {
+    let box = {
       xMin: SIZE + 1,
       xMax: 0,
       yMin: SIZE + 1,
       yMax: 0
-    };
+    }; //301,0,301,0
 
     for (let y = 1; y <= SIZE; y++) {
       for (let x = 1; x <= SIZE; x++) {
         if (matrix[y][x] == 0) {
-          bbox.yMin = Math.min(y, bbox.yMin);
-          bbox.yMax = Math.max(y, bbox.yMax);
-          bbox.xMin = Math.min(x, bbox.xMin);
-          bbox.xMax = Math.max(x, bbox.xMax);
+          box.yMin = Math.min(y, box.yMin);
+          box.yMax = Math.max(y, box.yMax);
+          box.xMin = Math.min(x, box.xMin);
+          box.xMax = Math.max(x, box.xMax);
         }
       }
     }
 
-    return bbox;
+    return box;
   }
 
   drawBox(box) {
@@ -213,7 +220,7 @@ export default class PictureBot {
     }
   }
 
-  getPixelMatrix(dataArray) {
+  getGreyScaleMatrix(dataArray) {
     let matrix = [];
     for (let i = 1; i <= SIZE; i++) {
       matrix[i] = [];
